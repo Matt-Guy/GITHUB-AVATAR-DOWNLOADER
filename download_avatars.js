27687ddac3642b6c110a450b27f7267a46bb4a33
Matt-Guy <guy.mtsh@gmail.com>
2017-11-07 17:14:01 +0000

    downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,30 @@
-var request = require('request');
+var secret = require('./secrets.js')
 
-console.log('Welcome to the GitHub Avatar Downloader');
+function getRepoContributors(repoOwner, repoName, cb) {
+
+ console.log('Welcome to the GitHub Avatar Downloader!');
+
+ var request = require('request');
+
+
+var options = {
+
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent' : secret.GITHUB_TOKEN
+  }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
