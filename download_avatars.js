b3f6a5a55eecd221bc6a4ab982ea52a1008bc6ed
Matt-Guy <guy.mtsh@gmail.com>
2017-11-07 17:47:05 +0000

    Step 7

--- download_avatars.js
+++ download_avatars.js
@@ -16,7 +16,11 @@ var options = {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    if(err) {
+      cb(err)
+    } else {
+      cb(null, JSON.parse(body));
+    }
   });
 }
 
@@ -25,6 +29,14 @@ var options = {
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  if (err) {
+    console.error("Request function failed");
+    return
+  }
+  for (let i = 0; i < result.length; i++ ) {
+      console.log(result[i].avatar_url)
+  }
+  //console.log("Errors:", err);
+  //console.log("Result:", result);
+  console.log()
 });
