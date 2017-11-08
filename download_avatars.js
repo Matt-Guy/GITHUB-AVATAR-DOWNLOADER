3df86a1149b286f6860ac3f57f368467ea792959
Matt-Guy <guy.mtsh@gmail.com>
2017-11-08 02:36:58 +0000

    Avatar Downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,28 @@
 var secret = require('./secrets.js')
+var request = require('request');
+var fs = require('fs')
+
+
+var repowner = process.argv[2];
+var repname = process.argv[3];
+
+
+if ((repname && reowner) !== undefined){
+
+  function downloadImageByURL(url, filePath) {
+
+
+    request.get(url)
+       .on('error', function (err) {                                   // Note 2
+          throw err;
+     })
+
+  .pipe(fs.createWriteStream(filePath));
+
+}
+
+
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -9,34 +33,40 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 var options = {
 
-  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  url: "https://api.github.com/repos/" + repowner + "/" + repname+ "/contributors",
   headers: {
     'User-Agent' : secret.GITHUB_TOKEN
-  }
-  };
+    }
+
+};
+
+
 
   request(options, function(err, res, body) {
     if(err) {
       cb(err)
     } else {
-      cb(null, JSON.parse(body));
-    }
+        cb(null, JSON.parse(body));
+     }
   });
 }
 
 
 
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(repowner, repname, function(err, result) {
   if (err) {
     console.error("Request function failed");
     return
   }
-  for (let i = 0; i < result.length; i++ ) {
-      console.log(result[i].avatar_url)
+    for (let i = 0; i < result.length; i++ ) {
+       let url = result[i].avatar_url;
+       let filePath = `avatars/${result[i].login}.jpg`
+       downloadImageByURL(url, filePath)
   }
-  //console.log("Errors:", err);
-  //console.log("Result:", result);
-  console.log()
+
 });
+
+
+} else {
+  console.log('!!!ERROR!!! repname and repowner not specified')
+};
